3d8f5a6351f53ab40aa5b5fc80c0f032dfc2d702
markt5660 <mark.thew@gmail.com>
2020-07-05 09:58:03 -0700

    Latest updte

--- Gurps/planetaryobject.js
+++ Gurps/planetaryobject.js
@@ -197,24 +197,165 @@ var createSize = function (star, stellarOrbit, planetaryClass) {
   }
 }
 
+/**
+ * Determine satellite counts for planetary body
+   * (GT: First In, Ch 4, Step 10: Place Moons
+
+ * @param {*} star 
+ * @param {*} stellarOrbit 
+ * @param {*} planetaryClass 
+ * @param {*} planetaryDiameter 
+ */
 var createSatellites = function (star, stellarOrbit, planetaryClass, planetaryDiameter) {
   var d6 = dice.createD6()
 
   var innerFamilyCount = 0
+  var innerFamilyMod = 0
+  var innerFamilyRoll = 0
   var middleFamilyCount = 0
+  var middleFamilyMod = 0
+  var middleFamilyRoll = 0
   var outerFamilyCount = 0
   var outerFamilyMod = 0
+  var outerFamilyRoll = 0
 
+  var middleFamilyOrbits = []
+
+  // Pg 60, fill orbits with gas giants
   if (planetaryClass === objects.PlanetaryClassEnum.GAS_GIANT) {
-    innerFamilyCount = d6(2)
+  
+    // Set modifiers
+    if (stellarOrbit.radius < 0.5) {
+      middleFamilyMod += -5
+      outerFamilyMod += -5
+    } else if (stellarOrbit.radius < 0.75) {
+      middleFamilyMod += -3
+      outerFamilyMod += -3
+    } else if (stellarOrbit.radius < 1.5) {
+      middleFamilyMod += -1
+      outerFamilyMod += -1
+    }
 
-    outerFamilyCount = d6(1) + outerFamilyMod
+    // Modifier not currently supported: nearby planetary belts (page 61)
+    // if (a planetary belt (class "B") within 3 AU)
+    //    outerFamilyMod += 4
+
+    innerFamilyRoll = d6(2)
+    innerFamilyCount = innerFamilyRoll
+
+    middleFamilyRoll = d6(1)
+    middleFamilyCount = middleFamilyRoll + middleFamilyMod
+    if (middleFamilyCount < 0)
+      middleFamilyCount = 0
+
+    outerFamilyRoll = d6(1)
+    outerFamilyCount = outerFamilyRoll + outerFamilyMod
+    if (outerFamilyCount < 0)
+      outerFamilyCount = 0
+  
+    // Middle Family orbits/sizes
+    for (i=0; i < middleFamilyCount; i++) {
+      var moonClass = ''
+      var moonRadius = 0
+      var moonSize = 0
+
+      // Size of large moon
+      var moonSizeMod = 0
+      if (planetaryDiameter < 2000)
+        moonSizeMod = -5
+      else if (planetaryDiameter < 4000)
+        moonSizeMod = -4
+      else if (planetaryDiameter < 6000)
+        moonSizeMod = -3
+      else if (planetaryDiameter < 8000)
+        moonSizeMod = -2
+      else if (planetaryDiameter < 10000)
+        moonSizeMod = -1
+      else if (planetaryDiameter > 60000)
+        moonSizeMod = 2
+      else if (planetaryDiameter > 40000)
+        moonSizeMod = 1
+
+      var moonSizeRoll = d6(2) - 7
+      moonSize = moonSizeRoll + moonSizeMod
+      if (moonSize <= 0)
+        moonSize = 100
+      else
+        moonSize = moonSize * 1000
+
+      // Keep generating moon's radius until it is
+      // at least 2 radii away from every other moon
+      // (NOTE: limited to 20 attempts)
+      var radiusAttempts = 1
+      while (moonClass == '' && radiusAttempts < 20) {
+        var moonRadius = d6(3) + 3
+        if (moonRadius >= 15) moonRadius += d6(2)
+
+        moonClass = 'L'
+        for (j=0; j < middleFamilyOrbits.length && moonClass == 'L'; j++) {
+          var radiusDiff = Math.abs(middleFamilyOrbits[j].radius - moonRadius)
+          if (radiusDiff < 2) {
+            moonClass = ''
+          }
+        }
+
+        radiusAttempts += 1
+      }
+      
+      middleFamilyOrbits.push({ 
+        radius: moonRadius, 
+        class: moonClass, 
+        size: moonSize
+      })
+    }
+
+    // Sort middle family results by radius
+    middleFamilyOrbits.sort((orbit1, orbit2) => {
+      return orbit1.radius - orbit2.radius
+    })
+
+  } else if (planetaryClass === objects.PlanetaryClassEnum.TERRESTRIAL) {
+  
+    // Set modifiers
+    if (stellarOrbit.radius < 0.5)
+      middleFamilyMod += -99 // should generate no moons
+    else if (stellarOrbit.radius < 0.75) 
+      middleFamilyMod += -3
+    else if (stellarOrbit.radius < 1.5)
+      middleFamilyMod += -1
+    
+    if (planetaryDiameter < 2000)
+      middleFamilyMod += -2
+    else if (planetaryDiameter < 4000)
+      middleFamilyMod += -1
+    else if (planetaryDiameter > 9000)
+      middleFamilyMod += 1
+
+    // Number of large Moons
+    middleFamilyRoll = d6(1) - 4
+    middleFamilyCount = middleFamilyRoll + middleFamilyMod
+    if (middleFamilyCount < 0) {
+      middleFamilyCount = d6(1) - 2 + middleFamilyMod
+    }
+
+    // If no large moons, number of small moons
+    if (middleFamilyCount < 0) {
+      middleFamilyCount = 0
+    }
+  
   }
 
   return {
+    innerFamilyMod: innerFamilyMod,
+    innerFamilyRoll: innerFamilyRoll,
     innerFamilyCount: innerFamilyCount,
+    middleFamilyMod: middleFamilyMod,
+    middleFamilyRoll: middleFamilyRoll,
     middleFamilyCount: middleFamilyCount,
-    outerFamilyCount: outerFamilyCount
+    outerFamilyMod: outerFamilyMod,
+    outerFamilyRoll: outerFamilyRoll,
+    outerFamilyCount: outerFamilyCount,
+    middleFamilyOrbits: middleFamilyOrbits
   }
 }
 
